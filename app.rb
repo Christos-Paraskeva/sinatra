cfc2c302729909afab2500337e9b6838d12de7b1
Christos <chris.paraskeva10@gmail.com>
2017-02-28 12:53:18 +0000

    added if statement to website arguments

--- app.rb
+++ app.rb
@@ -12,8 +12,17 @@ get '/secret/bug' do
   "There's a spider on your face, Chris. Look! Hello? "
 end
 
-get '/cat' do
+get '/random-cat' do
   @name = ["Amigo", "Oscar", "Viking"].sample
   @age = [24, 30, 95].sample
+  erb(:index)
+end
+
+get '/named-cat/' do
+  p params
+  #params[:age] = 33
+  p params
+  @name = params[:name]
+  @age = params[:age]
   erb(:index)
 end
